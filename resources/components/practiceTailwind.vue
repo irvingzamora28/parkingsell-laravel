aebb55b1010d1fba11175784c88c6618e4bfae87
Irving Zamora <irvingzamora28@gmail.com>
2022-11-30 22:49:51 -0700

    Add component to practice css tailwind

---
+++ resources/components/practiceTailwind.vue
@@ -0,0 +1,48 @@
+<style>
+    .flex-container {
+        border: 1px solid black;
+        background-color: darkgray;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        flex-flow: row wrap;
+        align-content: center;
+        margin: 0 auto;
+        width: 50%;
+        height: 500px;
+    }
+
+    .box{
+        border: 1px solid red;
+        background-color: aquamarine;
+        color:black;
+        font-weight: bold;
+        width: 120px;
+        height: 120px;
+        text-align: center;
+        margin: 5px;
+    }
+    .box1 {
+        background-color: lightblue;
+        flex-grow: 1;
+    }
+    .box2 {
+        background-color: lightcoral;
+        flex-grow: 2;
+    }
+    .box3 {
+        background-color: lightgreen;
+        flex-grow: 3;
+    }
+</style>
+
+<template>
+    <div>
+        <h1>Titulo 1</h1>
+        <section class="flex-container">
+            <div class="box box1">Box 1</div>
+            <div class="box box2">Box 2</div>
+            <div class="box box3">Box 3</div>
+        </section>
+    </div>
+</template>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,11 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import productIndex from "../../components/products/index.vue";
 import productCreate from "../../components/products/create.vue";
 import notFound from "../../components/404.vue";
+import practice from "../../components/practiceTailwind.vue"
 import { create } from "lodash";
 const routes = [
     {
         path: "/",
-        component: productIndex,
+        component: practice,
     },
     {
         path: "/product/create",
