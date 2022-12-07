69f48e3b160c713962506e3483d1efb17f257f54
Irving Zamora <irvingzamora28@gmail.com>
2022-12-06 21:51:51 -0600

    Add raw css from tailwind tutorial

--- resources/components/practiceTailwind.vue
+++ resources/components/practiceTailwind.vue
@@ -1,48 +1,59 @@
 <style>
-    .flex-container {
-        border: 1px solid black;
-        background-color: darkgray;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        flex-flow: row wrap;
-        align-content: center;
-        margin: 0 auto;
-        width: 50%;
-        height: 500px;
-    }
+.chat-notification {
+    display: flex;
+    max-width: 24rem;
+    margin: 0 auto;
+    padding: 1.25rem;
+    border-radius: 0.5rem;
+    background-color: aliceblue;
+    box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
+}
 
-    .box{
-        border: 1px solid red;
-        background-color: aquamarine;
-        color:black;
-        font-weight: bold;
-        width: 120px;
-        height: 120px;
-        text-align: center;
-        margin: 5px;
-    }
-    .box1 {
-        background-color: lightblue;
-        flex-grow: 1;
-    }
-    .box2 {
-        background-color: lightcoral;
-        flex-grow: 2;
-    }
-    .box3 {
-        background-color: lightgreen;
-        flex-grow: 3;
-    }
+.chat-notification-logo-wrapper {
+    flex-shrink: 0;
+}
+
+.chat-notification-logo {
+    width: 3rem;
+    height: 3rem;
+    margin-top: 0.25rem;
+}
+
+.chat-notification-content {
+    flex-shrink: 1;
+    margin: 0px;
+    padding: 0px;
+    margin-left: 1.5rem;
+}
+
+.chat-notification-title {
+    color: black;
+    font-size: 1.2rem;
+    margin: 0px;
+    padding: 0px;
+}
+
+.chat-notification-message {
+    color: grey;
+    font-size: 1rem;
+    line-height: 1.2rem;
+    padding-top: 0.25rem;
+}
 </style>
 
 <template>
     <div>
-        <h1>Titulo 1</h1>
-        <section class="flex-container">
-            <div class="box box1">Box 1</div>
-            <div class="box box2">Box 2</div>
-            <div class="box box3">Box 3</div>
-        </section>
+        Tailwind Tutorial
+        <br>
+        https://tailwindcss.com/docs/utility-first
+        <div class="chat-notification">
+            <div class="chat-notification-logo-wrapper">
+                <img src="https://picsum.photos/200" alt="Logo" class="chat-notification-logo">
+            </div>
+            <div class="chat-notification-content">
+                <h4 class="chat-notification-title">Title message</h4>
+                <p class="chat-notification-message">You have a new message!</p>
+            </div>
+        </div>
     </div>
 </template>
