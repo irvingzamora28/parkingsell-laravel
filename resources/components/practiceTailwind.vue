7a24761948a8ee0068423c6f264db2f44a51c27d
Irving Zamora <irvingzamora28@gmail.com>
2022-12-11 00:18:32 -0600

    Add tailwind practice hover classes

--- resources/components/practiceTailwind.vue
+++ resources/components/practiceTailwind.vue
@@ -1,73 +1,50 @@
-<style>
-.chat-container {
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-}
-
-.chat-notification {
-    display: flex;
-    background-color: azure;
-    color: slategrey;
-    border-radius: 0.6rem;
-    margin-right: 1rem;
-    max-width: 24rem;
-    box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04);
-}
-
-.chat-notification-logo-wrapper {
-    height: 100%;
-    min-width: 5rem;
-    flex-shrink: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-}
-
-.chat-notification-content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    padding-right: 1rem;
-}
-
-.chat-notification-title {
-    font-size: 1.2rem;
-    font-weight: 500;
-    color: #404041;
-}
-
-.chat-notification-logo {
-    border-radius: 0.2rem;
-    width: 5rem;
-    margin: 0.5rem;
-}
-</style>
 
 <template>
     <div>
+        <!-- https://picsum.photos/200 -->
         <h1>Titulo 1</h1>
         <div class="chat-container">
-            <div class="chat-notification">
-                <div class="chat-notification-logo-wrapper">
-                    <img src="https://picsum.photos/200" alt="Logo" class="chat-notification-logo">
+
+            <div class="flex mx-auto items-center space-x-4 p-4 bg-gray-100 shadow-xl rounded-xl max-w-sm">
+                <div class="flex shrink-0">
+                    <img class="h-14 w-14 rounded-md" src="http://picsum.photos/200" alt="Image">
                 </div>
-                <div class="chat-notification-content">
-                    <h4 class="chat-notification-title">ChitChat</h4>
-                    <p class="chat-notification-message">You have a new ,m ,cmzx,cmzx,cmzx ajds sjdhf jsdhf jsd hfjsd
-                        fjsd fjaksjjas dkjasdkj asd!</p>
+                <div class="">
+                    <div class="flex font-medium text-slate-900">
+                        You have a message
+                    </div>
+                    <p class="text-slate-600">Message askjdsakjd ksajdkasdjsa kdjaskdjsakjdkajs dkjasdkjaskj kj sdkjas dkj asdj askdj askjdsakjdksaj k</p>
+
                 </div>
             </div>
 
-            <div class="flex max-w-sm bg-slate-500 space-x-4 items-center rounded-xl p-2 shadow-lg hover:bg-slate-700 cursor-pointer">
-                <div class="flex w-16 h-16 shrink-0">
-                    <img src="https://picsum.photos/200" alt="Logo" class="rounded-md">
+            <div class="mt-8 flex p-4 space-x-4 max-w-sm mx-auto bg-slate-50 rounded-xl shadow-xl">
+                <div class="flex">
+                    <img class=" block rounded-full w-14 h-14" src="https://picsum.photos/200" alt="" srcset="">
                 </div>
-                <div class="border-red-100 flex items-start flex-col odd:bg-white">
-                    <h4 class="text-xl text-black">ChitChat</h4>
-                    <p class="text-slate-100">You have a new message!</p>
+                <div class="space-y-0.5 w-full">
+                    <p class="text-medium text-slate-800 text-lg">You have a message</p>
+                    <p class="text-medium text-slate-500">You have 400 new messages from your friends!</p>
+                    <div class="text text-right">
+                        <button class="bg-white border border-indigo-900 text-indigo-900 px-4 py-1 rounded-md shadow-md hover:text-white hover:bg-indigo-800 hover:shadow-2xl">OK</button>
+                    </div>
                 </div>
             </div>
+
+            <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+                <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://picsum.photos/200" alt="Woman's Face">
+                <div class="text-center space-y-2 sm:text-left">
+                  <div class="space-y-0.5">
+                    <p class="text-lg text-black font-semibold">
+                      Erin Lindford
+                    </p>
+                    <p class="text-slate-500 font-medium">
+                      Product Engineer
+                    </p>
+                  </div>
+                  <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+                </div>
+              </div>
         </div>
     </div>
 </template>
