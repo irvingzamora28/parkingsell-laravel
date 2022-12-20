5da7043bbd4bdbb80731433e688f9a6d69b0f3d8
Irving Zamora <irvingzamora28@gmail.com>
2022-12-20 11:58:09 -0600

    Add banner and animate text banner in practiceTailwind

--- resources/components/practiceTailwind.vue
+++ resources/components/practiceTailwind.vue
@@ -1,18 +1,18 @@
 
 <template>
-    <header class="bg-slate-700 font-sans text-slate-50 h-12">
-        <div class="flex container mx-auto w-4/5 h-full">
-            <div id="logo" class="flex items-center">
+    <header class="bg-slate-700 font-sans text-slate-50 md:h-12 h-36">
+        <div class="flex container mx-auto w-4/5 h-full md:flex-row flex-col">
+            <div id="logo" class="flex items-center justify-center mx-auto flex-1 md:flex-grow-0">
                 <div class="flex w-48 h-8 px-8 items-center space-x-4 justify-center
                     text-center bg-slate-400 tracking-wider cursor-pointer overflow-hidden
                     transition duration-300 rounded-sm
-                    hover:bg-slate-800 hover:text-slate-50">
-                    <i class="fa-solid fa-gear block float-left text-2xl"></i>
-                    <h3 class="block float-right text-2xl drop-shadow-2xl">TAILWIND</h3>
+                    hover:bg-slate-800 hover:text-slate-50 group">
+                    <i class="fa-solid fa-gear block float-left text-2xl animate-spin-slow group-hover:animate-none group-hover:animate-from-bottom"></i>
+                    <h3 class="block float-right text-2xl drop-shadow-2xl group-hover:animate-from-right">TAILWIND</h3>
                 </div>
             </div>
-            <nav class="flex w-full h-full border border-red-400">
-                <ul class="flex w-full items-center justify-end text-2xl space-x-4 tracking-wider">
+            <nav class="flex w-full h-full flex-1">
+                <ul class="flex w-full items-center justify-end text-2xl space-x-4 tracking-wide">
                     <li class="menu-item"><a class="" href="#">INICIO</a></li>
                     <li class="menu-item"><a class="" href="#">BLOG</a></li>
                     <li class="menu-item"><a class="" href="#">FORMACION</a></li>
@@ -21,4 +21,12 @@
             </nav>
         </div>
     </header>
+
+    <section class="container w-11/12 mx-auto">
+        <div id="banner" class="w-full h-28 mx-auto border-8 border-white overflow-hidden
+        bg-banner bg-banner-position animate-bg-banner shadow m-5">
+            <h1 class="block text-banner text-center text-slate-50
+            font-normal tracking-wider m-7 animate-text-banner text-lg md:text-4xl">TAILWIND PRACTICE</h1>
+        </div>
+    </section>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-        backgroundImage: theme => ({
-            'body-pattern' : "url('../img/pattern.png')",
-        })
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                "body-pattern": "url('../img/pattern.png')",
+                banner: "url(../img/backbanner.png)",
+            }),
+            backgroundPosition: {
+              "banner-position": "-12rem -12rem"
+            },
+            animation: {
+                "spin-slow": "spin 3s linear infinite",
+                "from-bottom": "fromBottom 500ms linear",
+                "from-right": "fromRight 500ms linear",
+                "bg-banner": "backBanner 20s linear infinite",
+                "text-banner": "showBannerText 8s linear",
+            },
+            keyframes: {
+                fromBottom: {
+                    "0%": { transform: "translateY(0%)" },
+                    "50%": { transform: "translateY(200%)" },
+                    "100%": { transform: "translateY(0%)" },
+                },
+                fromRight: {
+                    "0%": { transform: "translateX(200%)" },
+                    "100%": { transform: "translateX(0%)" },
+                },
+                backBanner: {
+                  "0%": {"background-position": "0rem 0rem"},
+                  "100%": {"background-position": "-18rem -22rem"},
+                },
+                showBannerText: {
+                  "0%": { transform: "translateX(-600%) scale(5,5)",
+                          opacity: 1 },
+                          "50%": { transform: "translateX(600%) scale(5,5)",
+                          opacity: 1 },
+                          "75%": { transform: "translateX(-600%) scale(5,5)",
+                          opacity: 0,
+                        "text-shadow": "none",
+                      color: "transparent" },
+                      "100%": { transform: "translateX(0%) scale(1,1)",
+                          opacity: 1,
+                        "text-shadow": "2px 2px 1px black",
+                      color: "white" },
+
+                }
+            },
+        },
     },
-  },
-  plugins: [],
-}
+    plugins: [],
+};
