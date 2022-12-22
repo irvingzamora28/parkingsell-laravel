3f0455308f17cb769515fc7d7e5936f7b274a28c
Irving Zamora <irvingzamora28@gmail.com>
2022-12-22 00:54:26 -0700

    PracticeTailwind Make responsive cards Add side section

--- resources/components/practiceTailwind.vue
+++ resources/components/practiceTailwind.vue
@@ -7,7 +7,8 @@
                     text-center bg-slate-400 tracking-wider cursor-pointer overflow-hidden
                     transition duration-300 rounded-sm
                     hover:bg-slate-800 hover:text-slate-50 group">
-                    <i class="fa-solid fa-gear block float-left text-2xl animate-spin-slow group-hover:animate-none group-hover:animate-from-bottom"></i>
+                    <i
+                        class="fa-solid fa-gear block float-left text-2xl animate-spin-slow group-hover:animate-none group-hover:animate-from-bottom"></i>
                     <h3 class="block float-right text-2xl drop-shadow-2xl group-hover:animate-from-right">TAILWIND</h3>
                 </div>
             </div>
@@ -29,36 +30,99 @@
             font-normal tracking-wider m-7 animate-text-banner text-lg md:text-4xl">TAILWIND PRACTICE</h1>
         </div>
 
-        <div id="cards-container" class="flex flex-nowrap justify-between">
-            <div class="card">
+        <!-- <div id="cards-container" class="flex flex-nowrap justify-between"> -->
+        <div id="cards-container"
+            class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 sm:gap-12 lg:grid-cols-5 lg:gap-16 justify-between">
+
+            <div class="card group">
                 <p class="card-icon"> <i class="fas fa-code fa-fw"></i> </p>
                 <h2 class="card-category">DESARROLLO</h2>
                 <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis</p>
             </div>
 
-            <div class="card">
+            <div class="card group">
                 <p class="card-icon"> <i class="fas fa-terminal fa-fw"></i> </p>
                 <h2 class="card-category">SISTEMAS OPERATIVOS</h2>
                 <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis</p>
             </div>
 
-            <div class="card">
+            <div class="card group">
                 <p class="card-icon"> <i class="fa-solid fa-computer"></i> </p>
                 <h2 class="card-category">HARDWARE</h2>
                 <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis</p>
             </div>
 
-            <div class="card">
+            <div class="card group">
                 <p class="card-icon"> <i class="fa-solid fa-network-wired"></i> </p>
                 <h2 class="card-category">REDES E INTERNET</h2>
                 <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis</p>
             </div>
 
-            <div class="card">
+            <div class="card group">
                 <p class="card-icon"> <i class="fa-solid fa-database"></i> </p>
                 <h2 class="card-category">REDES E INTERNET</h2>
                 <p class="card-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis</p>
             </div>
         </div>
     </section>
+
+    <main>
+        <aside id="side-section">
+            <h3>BUSCAR</h3>
+            <div id="search_box">
+                <form action="">
+                    <input type="text" name="search" id="input_search">
+                    <input type="button" value="Buscar">
+                </form>
+            </div>
+
+            <h3>LOGIN</h3>
+            <div id="login_box">
+                <form action="">
+                    <label for="email">Usuario</label>
+                    <input type="email" name="email" id="input_email">
+
+                    <label for="password">Contrasena</label>
+                    <input type="password" name="password" id="input_password">
+                    <input type="submit" value="Entrar">
+                    <input type="reset" value="Limpiar">
+
+                    <a href="#">Registrate aqui</a>
+                    <a href="#">Has olvidado tu contrasena?</a>
+                </form>
+            </div>
+
+            <h3>REDES SOCIALES</h3>
+            <div id="social_media_box">
+                <div class="twitter_box">
+                    <a href="#">Twitter</a>
+                    <p>Twitter</p>
+                </div>
+                <div class="facebook_box">
+                    <a href="#">Facebook</a>
+                    <p>Facebook</p>
+                </div>
+                <div class="youtube_box">
+                    <a href="#">Youtube</a>
+                    <p>Youtube</p>
+                </div>
+            </div>
+
+            <h3>PATROCINADORES</h3>
+            <div id="sponsors_box">
+            </div>
+        </aside>
+        <section>
+            <h2>Ultimos articulos</h2>
+            <article>
+                <div>
+                    <span>Fecha: 10 marz</span>
+                    <span>Categoria: Pruebas</span>
+                </div>
+
+                <h4> <a href="#">Titulo de articulo</a> </h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magni id odit facilis eum similique ducimus qui ea itaque vero.</p>
+            </article>
+        </section>
+    </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
                 banner: "url(../img/backbanner.png)",
             }),
             backgroundPosition: {
-              "banner-position": "-12rem -12rem"
+                "banner-position": "-12rem -12rem",
             },
             animation: {
                 "spin-slow": "spin 3s linear infinite",
@@ -20,6 +20,9 @@ module.exports = {
                 "from-right": "fromRight 500ms linear",
                 "bg-banner": "backBanner 20s linear infinite",
                 "text-banner": "showBannerText 8s linear",
+                "show-card-icon": "showCardIcon 300ms linear",
+                "show-card-category": "showCardCategory 300ms linear",
+                "show-card-description": "showCardDescription 300ms linear",
             },
             keyframes: {
                 fromBottom: {
@@ -32,23 +35,54 @@ module.exports = {
                     "100%": { transform: "translateX(0%)" },
                 },
                 backBanner: {
-                  "0%": {"background-position": "0rem 0rem"},
-                  "100%": {"background-position": "-18rem -22rem"},
+                    "0%": { "background-position": "0rem 0rem" },
+                    "100%": { "background-position": "-18rem -22rem" },
                 },
                 showBannerText: {
-                  "0%": { transform: "translateX(-600%) scale(5,5)",
-                          opacity: 1 },
-                          "50%": { transform: "translateX(600%) scale(5,5)",
-                          opacity: 1 },
-                          "75%": { transform: "translateX(-600%) scale(5,5)",
-                          opacity: 0,
+                    "0%": {
+                        transform: "translateX(-600%) scale(5,5)",
+                        opacity: 1,
+                    },
+                    "50%": {
+                        transform: "translateX(600%) scale(5,5)",
+                        opacity: 1,
+                    },
+                    "75%": {
+                        transform: "translateX(-600%) scale(5,5)",
+                        opacity: 0,
                         "text-shadow": "none",
-                      color: "transparent" },
-                      "100%": { transform: "translateX(0%) scale(1,1)",
-                          opacity: 1,
+                        color: "transparent",
+                    },
+                    "100%": {
+                        transform: "translateX(0%) scale(1,1)",
+                        opacity: 1,
                         "text-shadow": "2px 2px 1px black",
-                      color: "white" },
-
+                        color: "white",
+                    },
+                },
+                showCardIcon: {
+                    "0%": {
+                        transform: "translateY(-200%) scale(0.5,0.5)",
+                    },
+                    "100%": {
+                        transform: "translateY(0%) scale(1,1)"
+                    }
+                },
+                showCardCategory: {
+                    "0%": {
+                        transform: "translateY(-300%)",
+                    },
+                    "100%": {
+                        transform: "translateY(0%)"
+                    }
+                },
+                showCardDescription: {
+                    "0%": {
+                        transform: "translateX(-200%)",
+                    },
+                    "100%": {
+                        transform: "translateX(0%)"
+                    }
                 }
             },
         },
